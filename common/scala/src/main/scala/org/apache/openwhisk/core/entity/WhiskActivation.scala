368d14b1cfbda54e09524b73295d74f8e65cd42a
srost <srost@de.ibm.com>
2021-04-14 15:35:40 +0200

    adapt serdes to 14 json params

--- common/scala/src/main/scala/org/apache/openwhisk/core/entity/WhiskActivation.scala
+++ common/scala/src/main/scala/org/apache/openwhisk/core/entity/WhiskActivation.scala
@@ -180,7 +180,7 @@ object WhiskActivation
    */
   lazy val filtersView = WhiskQueries.view(WhiskQueries.dbConfig.activationsFilterDdoc, collectionName)
 
-  override implicit val serdes = jsonFormat13(WhiskActivation.apply)
+  override implicit val serdes = jsonFormat14(WhiskActivation.apply)
 
   // Caching activations doesn't make much sense in the common case as usually,
   // an activation is only asked for once.
